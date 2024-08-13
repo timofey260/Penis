2c7cc50774933ffb7bea875ed68576d774a03a8e
timofey26 <smailovtimofey@gmail.com>
2024-08-13 12:58:10 +0100

    almost almost done

--- Drizzle/Runtime.py
+++ Drizzle/Runtime.py
@@ -5,4 +5,5 @@ from Drizzle.Data.LingoPropertyList import LingoPropertyList
 from Drizzle.Data.LingoList import LingoList
 from Drizzle.LingoGlobal import LingoGlobal
 from Drizzle.Data.LingoImage import LingoImage
+from Drizzle.Data.LingoSymbol import LingoSymbol
 minimized = False
